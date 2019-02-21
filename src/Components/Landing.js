d8e4c315a9ffab45347a126017e862845877a9c0
Yekaterina Doros <yekaterinadoros@gmail.com>
2019-02-20 20:30:56 -0500

    readme

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
-import {Jumbotron}from 'react-bootstrap';
+
 
 class Landing extends Component{
     render(){
         return (
             <section id="landing">
-                <Jumbotron id="headline">
+                <section id="headline">
                     <span>Personal Training,< br /> Nutrition Coaching & <br />Competitive Bodybuilding</span>
-                </Jumbotron>
+                </section>
             </section>
         )
     }
