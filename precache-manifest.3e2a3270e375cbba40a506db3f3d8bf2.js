f4b20c1b465de51f6165e7f1b1b0e52778d7c983
Yekaterina Doros <yekaterinadoros@gmail.com>
2019-03-26 22:14:49 -0400

    Updates

---
+++ precache-manifest.3e2a3270e375cbba40a506db3f3d8bf2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbef4bdaf99bae6a9f2db2b413598ecb",
+    "url": "/frost-fitness-react/static/media/client2.fbef4bda.jpg"
+  },
+  {
+    "revision": "61cdc80d6f7d36a2f3c4",
+    "url": "/frost-fitness-react/static/css/main.041b9620.chunk.css"
+  },
+  {
+    "revision": "943a1c1aefc16b612422",
+    "url": "/frost-fitness-react/static/js/runtime~main.943a1c1a.js"
+  },
+  {
+    "revision": "dca3c3b886881780fb7b",
+    "url": "/frost-fitness-react/static/js/2.dca3c3b8.chunk.js"
+  },
+  {
+    "revision": "6a27a7f93682ed223c9152d0288b792c",
+    "url": "/frost-fitness-react/static/media/plate.6a27a7f9.png"
+  },
+  {
+    "revision": "5e35abb8533876dda1e6d75c0e9a5f80",
+    "url": "/frost-fitness-react/static/media/crossfit.5e35abb8.png"
+  },
+  {
+    "revision": "0604884ed391b8aee4e384c5774f60ed",
+    "url": "/frost-fitness-react/static/media/client1.0604884e.jpg"
+  },
+  {
+    "revision": "61cdc80d6f7d36a2f3c4",
+    "url": "/frost-fitness-react/static/js/main.61cdc80d.chunk.js"
+  },
+  {
+    "revision": "6c28430a3914bae95c32795ebf9fedc5",
+    "url": "/frost-fitness-react/static/media/client3.6c28430a.jpg"
+  },
+  {
+    "revision": "2531c5addcd7a3b30c6a50f1ab11fcd9",
+    "url": "/frost-fitness-react/static/media/fruit.2531c5ad.jpg"
+  },
+  {
+    "revision": "3e8b53c67c1de2cd57f707bb25827638",
+    "url": "/frost-fitness-react/static/media/dumbbells.3e8b53c6.jpg"
+  },
+  {
+    "revision": "154e65ad626aab35f569af28eb429932",
+    "url": "/frost-fitness-react/static/media/sport.154e65ad.jpg"
+  },
+  {
+    "revision": "a837f10c93afada62207376b48441ed3",
+    "url": "/frost-fitness-react/static/media/background.a837f10c.jpg"
+  },
+  {
+    "revision": "23b427de9cdb1ebb79ae73a9ba0ba275",
+    "url": "/frost-fitness-react/index.html"
+  }
+];
